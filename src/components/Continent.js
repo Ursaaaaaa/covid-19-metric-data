dc32e77a2d54b22f9ab04c74fd9a517261ff7e88
ramrod433 <djoletoamanor@gmail.com>
2022-06-23 17:12:56 +0000

    Added API and styles

---
+++ src/components/Continent.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+import images from '../images/images';
+
+const Continent = ({ continent: { continent, cases } }) => (
+  <Link
+    to={`/${continent}`}
+    data-testid="continentItem"
+    className="continentElement"
+  >
+    <BsFillArrowUpRightCircleFill className="arrowButton" />
+    <img
+      src={images[continent]}
+      alt="continent map"
+      className="continentImage"
+    />
+    <div className="continentText">
+      <h4 className="self-center md:self-end">
+        <span className="text-2xl">{`${continent}`}</span>
+      </h4>
+      <span className="text-xl">{cases.toLocaleString()}</span>
+    </div>
+  </Link>
+);
+
+Continent.propTypes = {
+  continent: PropTypes.shape({
+    continent: PropTypes.string.isRequired,
+    cases: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Continent;
---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import { useSelector } from 'react-redux';
+import CountUp from 'react-countup';
+import { FcGlobe } from 'react-icons/fc';
+import Spinner from './Spinner/Spinner';
+import ContinentList from './ContinentList';
+
+const Home = () => {
+  const {
+    continents,
+    totalData: { totalCases, totalDeaths },
+  } = useSelector((state) => state.covid);
+
+  return (
+    <>
+      {!continents.length && <Spinner />}
+      {continents.length > 0 && (
+        <>
+          <div className="globeDetails">
+            <FcGlobe className="globe" />
+            <div className="count">
+              <CountUp
+                delay={1}
+                end={totalCases}
+                separator=","
+                prefix="Total Cases: "
+                duration={3}
+              />
+              <CountUp
+                delay={1}
+                prefix="Total Deaths: "
+                end={totalDeaths}
+                separator=","
+                duration={3}
+              />
+            </div>
+          </div>
+          <ContinentList continents={continents} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
